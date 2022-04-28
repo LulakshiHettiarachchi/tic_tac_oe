6dac4ee9dd36acae3eaec7d057d200278316fc94
LulakshiHettiarachchi <lulakshinadunihettiarachchi@gmail.com>
2022-04-28 17:24:02 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Game from './components/Game/Game';
+
+function App() {
+  return (
+    
+   <Game></Game>
+  );
+}
+
+export default App;
---
+++ type.tsx
@@ -0,0 +1,4 @@
+import { type } from "@testing-library/user-event/dist/type";
+
+
+export type SquareValue = "x" | "o" | null;
